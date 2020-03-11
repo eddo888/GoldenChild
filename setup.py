1621e7d27822850b48f36bd50ceaf4e4ccd714b7
David Edson <dave@stibosystems.com.au>
2020-03-11 18:57:02 +1100

    python3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ pwd = path.abspath(path.dirname(__file__))
 with codecs.open(path.join(pwd, 'README.md'), 'r', encoding='utf8') as input:
     long_description = input.read()
 
-version='1.2'
+version='1.3'
 	
 setup(
 	name='GoldenChild',
@@ -25,7 +25,7 @@ setup(
 	],
 	install_requires=[
 		'argcomplete',
-		#'libxml2',
+		'libxml2-python3',
 		'xmltodict',
 		'Perdy',
 		'Argumental',
