14313b7f63f5c4384ff3008f50439bd898401f11
David Edson <dave@stibosystems.com.au>
2020-04-01 14:12:58 +1100

    with validation

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ pwd = path.abspath(path.dirname(__file__))
 with codecs.open(path.join(pwd, 'README.md'), 'r', encoding='utf8') as input:
     long_description = input.read()
 
-version='1.5'
+version='1.6'
 	
 setup(
 	name='GoldenChild',
@@ -34,5 +34,6 @@ setup(
 	scripts=[
 		'bin/xget.py',
 		'bin/xset.py',
+		'bin/validate.py',
 	],
 )
